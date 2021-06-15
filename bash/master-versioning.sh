7366d781df7f9128ea3a7ed16bfba4e3850f0ae7
cicd_user <cicd_user@ymcaret.org>
2021-06-15 17:31:24 -0600

    ssh fix

--- bash/master-versioning.sh
+++ bash/master-versioning.sh
@@ -1,6 +1,5 @@
 git config --global user.email "eliudiaz@gmail.com"
 git config --global user.name "eliudiaz"
-git config --global url."git@github.com:".insteadOf "https://github.com/"
 git fetch --all
 git checkout -b master origin/master
 mvn -B -e gitflow:release-start -Dmaven.test.skip=true
